1fee30a4d7802fab372d23a37e2dc68919d54722
tornikerogava <tokorogava@gmail.com>
2021-06-22 16:56:04 +0400

    added the first button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 
 import './App.css';
+import {Howl, Howler} from "howler"
+import {useState} from "react"
+import audioClips from "./AudioArray.js"
+
 
 function App() {
+  const [Volume, setVolume] = useState(0.5);
+  Howler.volume(Volume)
+  
+
+  const SoundPlay = (src) =>{
+    const sound = new Howl({
+      src
+    })
+    sound.play()
+  }
+
+  const RenderButtonAndSound = () =>{
+    return audioClips.map((soundObj, index) => {
+      return(
+        <button key={index} onClick={() => SoundPlay(soundObj.sound)}>
+          {soundObj.label}
+        </button>
+      )
+    })
+  }
+  
   return (
-    <>
-    </>
+    <div id="drum-machine">
+      <div id="display">
+        {RenderButtonAndSound()}
+      </div>
+    </div>
   );
 }
 
---
+++ src/AudioArray.js
@@ -0,0 +1,9 @@
+import test from './Audio/Dasani Kit/dasani7.wav'
+
+
+
+ const audioClips = [
+    {sound: test, label: "test"}
+]
+
+export default audioClips
